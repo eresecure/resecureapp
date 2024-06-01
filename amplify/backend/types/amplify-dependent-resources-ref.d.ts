738c602c638ccd9e638c86f06204e51a8c2859ac
eresecure <eled@resecure.co>
2024-06-01 15:03:15 +0100

    feat: new

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,8 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "resecurereactappda0a0af0": {
+    "resecurereactapp": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
